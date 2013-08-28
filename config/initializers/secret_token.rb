ee6c30e9d0da15e70435587f6610966d7f392fde
Yutaro Sugai <y.sugai@everyleaf.com>
2013-08-28 09:24:15 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'd0d28aba8196935b30f9624cf47041df8f7d53016e1643aae11cc598f49a8eccb0670188eb87f0213aee7b4da0633dd9b9128f1aea54219feb7dd51b09d5eaf2'
